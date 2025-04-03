eef8548cd453baaaf347e7c3ed426475571178c2
uchennodimDATAFI <uche.nnodim@thepalladiumgroup.com>
2025-04-03 15:47:32 +0100

    server commit

--- staging_records_ingestion/file_ingestion_process.py
+++ staging_records_ingestion/file_ingestion_process.py
@@ -2,24 +2,7 @@ import psycopg2
 from datetime import datetime
 from file_loader import FileLoader
 import configparser
-
-def read_db_config(filename='/home/lamisplus/database_credentials/config.ini', section='database'):
-    # Create a parser
-    parser = configparser.ConfigParser()
-    # Read the configuration file
-    parser.read(filename)
-    # Get section, default to database
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception(f'Section {section} not found in the {filename} file')
-    return db
-
-db_config = read_db_config()
-
+from database_connection.db_connect import connect_to_db
 
 def insert_pipeline_log(cur, log_id, start_time):
     insert_pipeline_query = """
@@ -37,22 +20,15 @@ def update_pipeline_log(cur, log_id, end_time, status, error_message=None, recor
     cur.execute(update_pipeline_query, (end_time, status, error_message, records_processed, log_id))
 
 def main():
-    db_params = {
-        'host': db_config['stg_host'],
-        'database': db_config['stg_database_name'],
-        'user': db_config['stg_username'],
-        'password': db_config['stg_password'],
-        'port': db_config['stg_port'],
-    }
-
+    
     try:
-        with psycopg2.connect(**db_params) as conn:
-            with conn.cursor() as cur:
+        with connect_to_db.connect('lamisplus_staging_dwh')[0] as filedb_conn:
+            with filedb_conn.cursor() as filedb_cur:
                 start_time = datetime.now() 
                 log_id = f'IPID_{start_time.strftime("%Y%m%d_%H_%M")}' #ingestion process ID
                 print(log_id)
                 
-                insert_pipeline_log(cur, log_id, start_time)
+                insert_pipeline_log(filedb_cur, log_id, start_time)
                 
                 try:
                     print('Job Started')
@@ -65,16 +41,16 @@ def main():
                         FROM file_ingestion_log 
                         WHERE load_start_time >= %s AND load_end_time <= %s
                     """
-                    cur.execute(q_check_count, (start_time, end_time))
-                    records_processed = cur.fetchone()[0]
+                    filedb_cur.execute(q_check_count, (start_time, end_time))
+                    records_processed = filedb_cur.fetchone()[0]
     
-                    update_pipeline_log(cur, log_id, end_time, 'Job Passed', 'No Errors', records_processed)
+                    update_pipeline_log(filedb_cur, log_id, end_time, 'Job Passed', 'No Errors', records_processed)
                     print('Job was run Successfully')
                 
                 except Exception as e:
                     error_msg = str(e)
                     end_time = datetime.now()
-                    update_pipeline_log(cur, log_id, end_time, 'Job Failed', error_msg)
+                    update_pipeline_log(filedb_cur, log_id, end_time, 'Job Failed', error_msg)
     
     except psycopg2.Error as e:
         print("Error connecting to PostgreSQL database:", e)
