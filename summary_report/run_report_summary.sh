77fd41ed0664b1a5e0e2d858d8f050b32b684bdd
Arshavin023 <uchejudennodim@gmail.com>
2026-05-18 08:35:27 +0100

    cleaned pipeline, dockerization

--- summary_report/run_report_summary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change to the directory where your Python script is located
-cd /home/oluwaloseyi
-
-# Run the Python script
-python3 pcs_summary_report.py
--- test_pgbouncer.py
+++ test_pgbouncer.py
@@ -1,13 +1,20 @@
 import asyncio
 import asyncpg
+import os
+
+
+db_host = os.environ.get("DB_HOST", "localhost")
+db_user = os.environ.get("DB_USER", "lamisplus")
+db_password = os.environ.get("DB_PASSWORD", "your_default_password")
+db_port = os.environ.get("DB_PORT", "5432")
 
 async def main():
     conn = await asyncpg.connect(
-        host="127.0.0.1",
+        host=db_host,
         port=6432, 
         database="lamisplus_staging_dwh",
-        user="lamisplus",
-        password="FmALa9PYGQUfyjq"
+        user=db_user,
+        password=db_password
     )
     result = await conn.fetch("SELECT NOW();")
     print(result)
