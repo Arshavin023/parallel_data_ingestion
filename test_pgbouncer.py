2bbff4e3cffc66429cc1a19bb1176c4bbcff46d6
Arshavin023 <uchejudennodim@gmail.com>
2026-05-20 11:58:34 +0100

    updated test_pgbouncer and requirements.txt

--- test_pgbouncer.py
+++ test_pgbouncer.py
@@ -6,12 +6,12 @@ import os
 db_host = os.environ.get("DB_HOST", "localhost")
 db_user = os.environ.get("DB_USER", "lamisplus")
 db_password = os.environ.get("DB_PASSWORD", "your_default_password")
-db_port = os.environ.get("DB_PORT", "5432")
+db_port = os.environ.get("DB_PORT", "6432")
 
 async def main():
     conn = await asyncpg.connect(
         host=db_host,
-        port=6432, 
+        port=db_port, 
         database="lamisplus_staging_dwh",
         user=db_user,
         password=db_password
